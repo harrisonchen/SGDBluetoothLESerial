b1aa5835e14813cf597302f9b0e86b560bdfe67e
Harrison Chen <harrisonxchen@gmail.com>
2015-02-05 11:44:46 -0800

    initial commit

---
+++ BluetoothLESerial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BluetoothLESerial
+//
+//  Created by Harrison Chen on 2/5/15.
+//  Copyright (c) 2015 StudentGlue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
